b59914b7500329e59e8fc2cd4daa89487c12ccc9
Reclipse <narcisprecup233@gmail.com>
2024-05-07 14:55:31 +0300

    Packages

--- src/main/java/com/reclipse/moofluids/Config.java
+++ src/main/java/com/reclipse/moofluids/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.reclipse.moofluids;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
--- src/main/java/com/reclipse/moofluids/MooFluids.java
+++ src/main/java/com/reclipse/moofluids/MooFluids.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.reclipse.moofluids;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
