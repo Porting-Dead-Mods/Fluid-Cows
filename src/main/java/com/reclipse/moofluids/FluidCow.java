0075b775062bbae8a009e5b2d460923091d8ff53
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-05-09 10:52:55 +0200

    Skill issue

--- src/main/java/com/reclipse/moofluids/FluidCow.java
+++ src/main/java/com/reclipse/moofluids/FluidCow.java
@@ -1,6 +1,7 @@
 package com.reclipse.moofluids;
 
 import com.reclipse.moofluids.utils.FluidCapacity;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -15,6 +16,14 @@ import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
+
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 public class FluidCow extends Cow {
@@ -28,18 +37,34 @@ public class FluidCow extends Cow {
 
     int milkingCooldown = MFConfig.defaultMilkingCooldown;
 
+    FluidTank cowTank = new FluidTank(Integer.MAX_VALUE){
+        @Override
+        public boolean isFluidValid(int tank, @NotNull FluidStack stack) {
+            return stack.getFluid().equals(Fluids.WATER);
+        }
+    };
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap) {
+        return LazyOptional.of(()->cowTank).cast();
+    }
+
     @Override
     public InteractionResult mobInteract(Player player, InteractionHand hand) {
         ItemStack itemstack = player.getItemInHand(hand);
 
-        if (0 <= FluidCapacity.getFluidCapacity(itemstack) && FluidCapacity.getFluidCapacity(itemstack) <= 1000) {
-            player.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
+        IFluidHandler cowTankHandler = (IFluidHandler) getCapability(null).orElseThrow(NullPointerException::new);
 
+        if(itemstack.is(Items.LAVA_BUCKET) && !this.isBaby()){
+            cowTankHandler.fill(new FluidStack(Fluids.LAVA, 1000), IFluidHandler.FluidAction.EXECUTE);
+            player.setItemInHand(hand, Items.BUCKET.getDefaultInstance());
+            System.out.println(cowTank.getFluid().getFluid().getFluidType());
         }
         if (itemstack.is(Items.BUCKET) && !this.isBaby()) {
             player.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
-            ItemStack fluidBucket = ItemUtils.createFilledResult(itemstack, player, Items.WATER_BUCKET.getDefaultInstance());
+            ItemStack fluidBucket = ItemUtils.createFilledResult(itemstack, player, Items.LAVA_BUCKET.getDefaultInstance());
             player.setItemInHand(hand, fluidBucket);
+            cowTankHandler.drain(new FluidStack(Fluids.LAVA,1000),IFluidHandler.FluidAction.EXECUTE);
             return InteractionResult.sidedSuccess(this.level().isClientSide);
         } else {
             return super.mobInteract(player, hand);
