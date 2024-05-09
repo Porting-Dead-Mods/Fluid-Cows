d887369419ad7f99b50a056bc22590b3f4ad4bbf
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-05-09 11:16:57 +0200

    Cow Rendering

--- src/main/java/com/reclipse/moofluids/renderer/FluidCowRenderer.java
+++ src/main/java/com/reclipse/moofluids/renderer/FluidCowRenderer.java
@@ -1,10 +1,24 @@
 package com.reclipse.moofluids.renderer;
 
-import net.minecraft.client.renderer.entity.CowRenderer;
+import com.reclipse.moofluids.FluidCow;
+import net.minecraft.client.model.CowModel;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
 
-public class FluidCowRenderer extends CowRenderer {
+public class FluidCowRenderer extends MobRenderer<FluidCow, CowModel<FluidCow>>
+{
+    private static final ResourceLocation COW_LOCATION = new ResourceLocation("textures/entity/cow/cow.png");
+
     public FluidCowRenderer(EntityRendererProvider.Context p_173956_) {
-        super(p_173956_);
+        super(p_173956_, new CowModel(p_173956_.bakeLayer(ModelLayers.COW)), 0.7F);
     }
+
+    @Override
+    public ResourceLocation getTextureLocation(FluidCow fluidCow) {
+        return COW_LOCATION;
+    }
+
+
 }
