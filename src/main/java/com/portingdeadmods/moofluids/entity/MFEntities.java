c28d1d7f910d84b376aa1ca7dcd1943c7fe3d1a3
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-08-24 18:07:48 +0200

    fix mob spawning

--- src/main/java/com/portingdeadmods/moofluids/entity/MFEntities.java
+++ src/main/java/com/portingdeadmods/moofluids/entity/MFEntities.java
@@ -12,7 +12,7 @@ import java.util.function.Supplier;
 public final class MFEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, MooFluids.MODID);
 
-    public static final Supplier<EntityType<FluidCow>> FLUID_COW = register("fluid_cow", EntityType.Builder.of(FluidCow::new, MobCategory.AMBIENT));
+    public static final Supplier<EntityType<FluidCow>> FLUID_COW = register("fluid_cow", EntityType.Builder.of(FluidCow::new, MobCategory.CREATURE));
 
     private static <T extends Entity> Supplier<EntityType<T>> register(String name, EntityType.Builder<T> builder) {
         return ENTITIES.register(name, () -> builder.build(MooFluids.MODID + ":" + name));
